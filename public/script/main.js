6b90ffdce08a072ef622ea93bae2ab0aa54dc4b2
Sivakeerthan Vamanarajasekaran <keerthan_1@hotmail.com>
2018-09-10 15:10:26 +0200

    scroll has to be fixed

--- public/script/main.js
+++ public/script/main.js
@@ -3,19 +3,21 @@
 //    window.open('../login.php');
 //});
 var lastScrollTop=0;
+$("#navbar").addClass('navbar-relative');
     $(window).scroll(function () {
-        var top = $('#navbar').offset().top;
-        if(window.pageYOffset > top){
-           $("#navbar").addClass('navbar-fixed');
+        var top = $("#navbar").offset().top;
+        if($(window).scrollTop() <= top){
+            $("#navbar").removeClass('navbar-relative');
+            $("#navbar").addClass('navbar-relative');
         }
-        if(window.pageYOffset <= top){
-            $("#navbar").removeClass('navbar-fixed');
-
+        if($(window).scrollTop() > top){
+            $("#navbar").removeClass('navbar-relative');
+            $("#navbar").addClass('navbar-fixed');
         }
         var st =$(this).scrollTop;
         if(st>lastScrollTop){
-            $(".logodiv").css('top',-((lastScrollTop/100)*st));
-            $(".logodiv").css('left',-((lastScrollTop/100)*st));
+            $(".logodiv").css('top',lastScrollTop-((lastScrollTop/100)*st));
+            $(".logodiv").css('left',lastScrollTop-((lastScrollTop/100)*st));
         }
         else{
 
