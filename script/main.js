be85413f0f8d77040e2257015a5052213d96f373
s.vamanarajasekaran <s.vamanarajasekaran@iet-gibb.ch>
2018-10-14 00:15:43 +0200

    little bit more responsive

--- script/main.js
+++ script/main.js
@@ -67,20 +67,22 @@ var lastScrollTop=0;
 
 //Changing BG-Image
 $(document).ready(function () {
-    var home = $("#Home").offset().bottom;
-    var about = $("#About").offset().top;
-    var files = $("#Files").offset().top;
-    var wst = $(window).scrollTop;
 
-    if(wst<=home){
-        $(".layer2").css('background-image','url("https://wallpapershome.com/images/wallpapers/landscape-3840x2160-flat-4k-5k-fog-iphone-wallpaper-forest-blue-11927.jpg")')
-    }
-    if(wst<=about){
-        $(".layer2").css('background-image','url("http://bdfjade.com/data/out/54/5612226-flat-wallpaper.jpg")')
-    }
-    if(wst<=files){
-        $(".layer2").css('background-image','url("https://i0.wp.com/get.wallpapers8k.com/wallpapers/a/2/3/90096.jpg")')
-    }
+    $("nav").click(function () {
+        if($("#About").visible(true)){
+            $(".layer2").css('background-image','url("https://wallpapershome.com/images/wallpapers/landscape-3840x2160-flat-4k-5k-fog-iphone-wallpaper-forest-blue-11927.jpg")')
+        }
+        if($("#Projects").visible(true)){
+            $(".layer2").css('background-image','url("http://bdfjade.com/data/out/54/5612226-flat-wallpaper.jpg")')
+        }
+        if($("#Files").visible(true)){
+            $(".layer2").css('background-image','url("https://i0.wp.com/get.wallpapers8k.com/wallpapers/a/2/3/90096.jpg")')
+        }
+        if($("#Contact").visible(true)){
+            $(".layer2").css('background-image','url("https://i0.wp.com/get.wallpapers8k.com/wallpapers/a/2/3/90096.jpg")')
+        }
+    });
+
 
 });
 
@@ -90,16 +92,16 @@ $(document).ready(function() {
     $('.tabs').tabs();
 
     $('.progress-li').mouseenter(function () {
-        var title = $('this ').children('.determinate').attr("title");
-        var value = $('this').children('span').val();
+        var title = $(this ).find('.determinate').attr("title");
+        var value = $(this).find('span').text();
         console.log("Title:"+title+"<br>Value:"+value);
-        $('this').children('.determinate').attr("title",value);
-        $('this').children('span').val(title);
+        $(this).find('.determinate').attr("title",value);
+        $(this).find('span').text(title);
     }).mouseleave(function () {
-        var title2 = $('this').children('.determinate').attr("title");
-        var value2 = $('this').children('span').val();
-        $('this').children('.determinate').attr("title",value2);
-        $('this').children('span').val(title2);
+        var title2 = $(this).find('.determinate').attr("title");
+        var value2 = $(this).find('span').text();
+        $(this ).find(".determinate").attr("title",value2);
+        $(this).find("span").text(title2);
     });
 
 });
@@ -116,23 +118,13 @@ $('.carousel.carousel-slider').carousel({
 //Password Validator
 
 function validatePW() {
-    if($("#pw-input").val() == "gibbiX12345"){
+    var corr = "gibbiX12345";
+    var val = $("#pw-input").val();
+    if(val === corr){
         M.toast({html: "Zugriff erfolgreich!"});
         $(".secure-form").css("display","none")
         $(".hidden-files").css("display","block");
-        do{
-            $(".hidden-files").html('  <div class="preloader-wrapper small active">\n' +
-                '    <div class="spinner-layer spinner-green-only">\n' +
-                '      <div class="circle-clipper left">\n' +
-                '        <div class="circle"></div>\n' +
-                '      </div><div class="gap-patch">\n' +
-                '        <div class="circle"></div>\n' +
-                '      </div><div class="circle-clipper right">\n' +
-                '        <div class="circle"></div>\n' +
-                '      </div>\n' +
-                '    </div>\n' +
-                '  </div>');
-        }while($(".hidden-files").has(".row"));
+
         try {
             $.get("templates/download-cards.html", function (temp_string) {
                 if(temp_string!=null){
@@ -148,6 +140,19 @@ function validatePW() {
             console.log("JQuery $.get Error:"+e.message);
         }
 
+        $(window).load(function () {
+            $(".hidden-files").html('  <div class="preloader-wrapper small active">\n' +
+                '    <div class="spinner-layer spinner-green-only">\n' +
+                '      <div class="circle-clipper left">\n' +
+                '        <div class="circle"></div>\n' +
+                '      </div><div class="gap-patch">\n' +
+                '        <div class="circle"></div>\n' +
+                '      </div><div class="circle-clipper right">\n' +
+                '        <div class="circle"></div>\n' +
+                '      </div>\n' +
+                '    </div>\n' +
+                '  </div>');
+        });
 
     }
     else{
@@ -177,5 +182,10 @@ if(getUrlParameter("result")=="success") {
     M.toast({html: "Ihre Nachricht wurde erfolgreich gesendet!"});
 }
 
+//Responsive Sidenav
+$(document).ready(function () {
+   $('.sidenav').sidenav();
+    $('.sidenav > .tab').tabs();
+});
 
 
